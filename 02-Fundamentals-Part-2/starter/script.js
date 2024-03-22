2585121acc6102310ba29eff0e2134e243eadb06
Daniel Yoo <dacapo0509@gmail.com>
2024-03-22 16:59:45 -0700

    functions calling other functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -13,11 +13,11 @@ function logger() {
 // Calling,running or invoking the function
 logger();
 
-function fruitProcessor(apples, oranges) {
-	console.log(apples, oranges);
-	const juice = `Juice with ${apples} apples and ${oranges} oranges`;
-	return juice;
-}
+// function fruitProcessor(apples, oranges) {
+// 	console.log(apples, oranges);
+// 	const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+// 	return juice;
+// }
 
 const appleJuice = fruitProcessor(5, 0);
 console.log(appleJuice);
@@ -46,13 +46,22 @@ console.log(age2);
 const calcAge3 = (birthYear) => {
 	return 2037 - birthYear;
 };
-
-const age3 = calcAge3(1991);
-console.log(age3, "arrow function");
-
 const yearsUntilRetirement = (year, name) => {
 	const age = 2023 - year;
 	return `${65 - age} is how many years ${name} has until retirement.`;
 };
 
-console.log(yearsUntilRetirement(1991, "Dan"));
+// FUNCTIONS CALLING OTHER FUNCTIONS
+
+function cutFruit(fruit) {
+	return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+	const applePieces = cutFruit(apples);
+	const orangePieces = cutFruit(oranges);
+	const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange`;
+	return juice;
+}
+
+console.log(fruitProcessor(3, 2));
