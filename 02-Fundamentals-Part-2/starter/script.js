254bd9f088c8653aa0ae9ad717da546db95bff88
Daniel Yoo <dacapo0509@gmail.com>
2024-03-22 16:47:05 -0700

    function declaration vs function expression

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,7 +1,39 @@
 "use strict";
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log("I can drive");
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log("I can drive");
+
+function logger() {
+	console.log("My name is Jonas");
+}
+
+// Calling,running or invoking the function
+logger();
+
+function fruitProcessor(apples, oranges) {
+	console.log(apples, oranges);
+	const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+	return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+//Function declaration
+function calcAge1(birthYear) {
+	return 2024 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+console.log(age1);
+
+//  Function expression
+const calcAge2 = function (birthYear) {
+	return 2024 - birthYear;
+};
+
+const age2 = calcAge2(1991);
+console.log(age2);
