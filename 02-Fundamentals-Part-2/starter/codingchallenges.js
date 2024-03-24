7da87be62125f405f24d3afae6bd3fb12b347950
Daniel Yoo <dacapo0509@gmail.com>
2024-03-24 13:51:29 -0700

    challenge 4 complete

--- 02-Fundamentals-Part-2/starter/codingchallenges.js
+++ 02-Fundamentals-Part-2/starter/codingchallenges.js
@@ -23,22 +23,48 @@ checkWinner(scoreDolphins, scoreKoalas);
 
 // CHALLENGE 2
 
-const calcTip = (billValue) => {
-	if (billValue >= 50 && billValue <= 300) {
-		return billValue * 0.15;
-	} else {
-		return billValue * 0.2;
-	}
+// const calcTip = (billValue) => {
+// 	if (billValue >= 50 && billValue <= 300) {
+// 		return billValue * 0.15;
+// 	} else {
+// 		return billValue * 0.2;
+// 	}
+// };
+
+// console.log(calcTip(100));
+
+// let bills = [125, 555, 44];
+// let tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// let totals = [
+// 	calcTip(bills[0]) + bills[0],
+// 	calcTip(bills[1]) + bills[1],
+// 	calcTip(bills[2]) + bills[2],
+// ];
+
+// CHALLENGE 4
+
+const calcTip = function (bill) {
+	return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 };
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
 
-console.log(calcTip(100));
+for (let i = 0; i < bills.length; i++) {
+	const tip = calcTip(bills[i]);
+	tips.push(tip);
+	totals.push(tip + bills[i]);
+}
+console.log(bills, tips, totals);
 
-let bills = [125, 555, 44];
-let tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-let totals = [
-	calcTip(bills[0]) + bills[0],
-	calcTip(bills[1]) + bills[1],
-	calcTip(bills[2]) + bills[2],
-];
-
-// CHALLENGE 3
+const calcAverage = function (arr) {
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		// sum = sum + arr[i];
+		sum += arr[i];
+	}
+	return sum / arr.length;
+};
+console.log(calcAverage([2, 3, 7]));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
