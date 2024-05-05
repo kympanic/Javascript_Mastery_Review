d0be788b14557e737c6090d74ac1e02e78e462a4
Daniel Yoo <dacapo0509@gmail.com>
2024-05-04 23:25:43 -0700

    highscore functionality complete

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -12,10 +12,10 @@ console.log(document.querySelector('.guess').value);
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess);
 
   //No input
   if (!guess) {
@@ -24,6 +24,10 @@ document.querySelector('.check').addEventListener('click', function () {
     //Correct input
     document.querySelector('.message').textContent = '🚀 Correct Number!';
     document.querySelector('.number').textContent = secretNumber;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
